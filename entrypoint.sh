173c271629a565df1f31bd3e93d5fadc475880f5
Jesus Dominguez <j.dominguez@kodeline.com>
2021-10-10 07:26:51 +0200

    Add some info in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
 
 cd /app/
+echo  " node version:    $(node -v) \n" \
+  "npm version:     $(npm -v) \n" \
+  "yarn version:    $(yarn -v) \n" \
+  "debian version:  $(cat /etc/debian_version) \n" \
+  "user:            $(whoami) \n" \
+  "chrome:          $(google-chrome --version || true) \n" \
+  "firefox:         $(firefox --version || true) \n"
 cypress run --browser chrome $@
